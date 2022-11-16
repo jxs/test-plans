b6eee6f9777bc49368fb51ee702226f4fff5660e
João Oliveira <hello@jxs.pt>
2022-11-16 21:38:15 +0000

    composer: format code

--- composer/index.ts
+++ composer/index.ts
@@ -5,6 +5,7 @@ import toml from "@iarna/toml";
 
 const execFile = promisify(require("node:child_process").execFile);
 const writeFile = promisify(require("node:fs").writeFile);
+const unlink = promisify(require("node:fs").unlink);
 
 const dbName = "combinations.db";
 
@@ -16,19 +17,30 @@ class Instance {
     }
 }
 
-class Group {
-    id: string;
+class Params {
     transport: string;
     muxer: string;
-    instance: Instance;
 
-    constructor(id: string, transport: string, muxer: string, instance: Instance) {
-        this.id = id;
+    constructor(transport: string, muxer: string) {
         this.transport = transport;
         this.muxer = muxer;
-        this.instance = instance;
     }
+}
 
+class Group {
+    id: string;
+    test_params: Params;
+    instances: Instance;
+
+    constructor(
+        id: string,
+        instances: Instance,
+        test_params: Params
+    ) {
+        this.id = id;
+        this.instances = instances;
+        this.test_params = test_params;
+    }
 }
 
 class Run {
@@ -47,64 +59,67 @@ class Composition {
     constructor(runs: Run[]) {
         this.runs = runs;
     }
-
 }
 
 async function main() {
-  sqlite3.verbose();
+    sqlite3.verbose();
 
-  // Call sqlite to process the csv resource files and generate a database.
-  // We call the sqlite process instead of doing it here cause
-  // the dot commands are interpreted by the sqlite cli tool not sqlite itself,
-  // and it is a lot faster parsing the csv's.
-  const { stdout, stderr } = await execFile("sqlite3", [
-    dbName,
-    ".mode csv",
-    ".import transports.csv transports",
-    ".import muxers.csv muxers",
-  ]);
-  if (stderr != "") {
-    throw new Error(`Could not parse csv resources: ${stderr}`);
-  }
+    // Call sqlite to process the csv resource files and generate a database.
+    // We call the sqlite process instead of doing it here cause
+    // the dot commands are interpreted by the sqlite cli tool not sqlite itself,
+    // and it is a lot faster parsing the csv's.
+    const { stdout, stderr } = await execFile("sqlite3", [
+        dbName,
+        ".mode csv",
+        ".import transports.csv transports",
+        ".import muxers.csv muxers",
+    ]);
+    if (stderr != "") {
+        throw new Error(`Could not parse csv resources: ${stderr}`);
+    }
 
-  const db = await open({
-    filename: dbName,
-    driver: sqlite3.Database,
-  });
+    const db = await open({
+        filename: dbName,
+        driver: sqlite3.Database,
+    });
 
-  // Generate the testing combinations by SELECT'ing from both transports
-  // and muxers tables the distinct combinations where the transport and the muxer
-  // of the different libp2p implementations match.
-  const queryResults =
-    await db.all(`SELECT DISTINCT a.id as id1, b.id as id2, a.transport, ma.muxer
-                              FROM transports a, transports b, muxers ma, muxers mb
-                              WHERE a.id != b.id
-                              AND a.transport == b.transport
-                              AND a.id == ma.id
-                              AND b.id == mb.id
-                              AND ma.muxer == mb.muxer;`);
-  await db.close()
+    // Generate the testing combinations by SELECT'ing from both transports
+    // and muxers tables the distinct combinations where the transport and the muxer
+    // of the different libp2p implementations match.
+    const queryResults =
+        await db.all(`SELECT DISTINCT a.id as id1, b.id as id2, a.transport, ma.muxer
+                     FROM transports a, transports b, muxers ma, muxers mb
+                     WHERE a.id != b.id
+                     AND a.transport == b.transport
+                     AND a.id == ma.id
+                     AND b.id == mb.id
+                     AND ma.muxer == mb.muxer;`);
+    await db.close();
 
-  let output = new Composition([]);
+    let output = new Composition([]);
 
-  for (let row of queryResults) {
-      let instance = new Instance(1);
+    for (let row of queryResults) {
+        let instance = new Instance(1);
 
-      let group1 = new Group(row.id1, row.transport, row.muxer, instance)
-      let group2 = new Group(row.id2, row.transport, row.muxer, instance)
+        let test_params = new Params(row.transport, row.muxer)
+        let group1 = new Group(row.id1, instance, test_params);
+        let group2 = new Group(row.id2, instance, test_params);
 
-      let run = new Run(`${row.id1} x ${row.id2}`, [group1, group2]);
+        let run = new Run(`${row.id1} x ${row.id2}`, [group1, group2]);
 
-      output.runs.push(run);
-  }
+        output.runs.push(run);
+    }
 
-  await writeFile("composition.toml", toml.stringify(output as any));
+    // Write the TOML file and remove the database file to avoid corrupting
+    // future runs.
+    await writeFile("composition.toml", toml.stringify(output as any));
+    await unlink(dbName);
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((err) => {
+        console.error(err);
+        process.exit(1);
+    });
 
